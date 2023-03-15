264588f2a1f7598e73a9bdfc54299e9f1eabce78
Mahlet-assefa <mahletassefa07@gmail.com>
2023-03-15 14:14:34 -0700

    created pattern.cpp

---
+++ mahletassefa/c++ problem solutions/pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+int main() {
+//Write an application to print out the numbers 10 through 49 in the following manner
+int num;
+int row;
+    for (num=10;num<=19;num++){
+    cout << num;
+     }
+cout << endl;
+    for (num=20;num<=29;num++){
+    cout << num;
+     }
+
+cout << endl;
+     for (num=30;num<=39;num++){
+    cout << num;
+   }
+cout << endl;
+   for (num=40;num<=49;num++){
+    cout << num;
+   }
+cout << endl;
+}
