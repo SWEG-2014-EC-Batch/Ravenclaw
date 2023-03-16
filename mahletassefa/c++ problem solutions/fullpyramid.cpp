bfecd2efe89fb01b5e0b2f5dc1dde0a5f7a8a390
Mahlet-assefa <mahletassefa07@gmail.com>
2023-03-16 12:55:16 -0700

    updated full pyramid astrix pattern

--- mahletassefa/c++ problem solutions/fullpyramid.cpp
+++ mahletassefa/c++ problem solutions/fullpyramid.cpp
@@ -2,22 +2,28 @@
 using namespace std;
 int main(){
   int row;
-  int k;
+  int i;
+  int k=0;
   int space;
+  int j;
   cout << "enter the number of rows"<<endl;
   cin >> row;
-  for (i=1;i<=row;i++;k=0){
-      for (space=1;space<=row-i;space++)
-      { cout << " ";
+  for (i=1;i<=row;i++){
+      for (space=1;space<=row-i;++space){ 
+        cout << " ";
          }
-    if (k!= 2*i-1){
+         for(int j = i; j <= 2*i-1; ++j)
+            cout << "* ";
+
+        for(int j = 0; j < i-1; ++j)
+            cout << "* ";
+
+    while (k!= 2*i-1){
         cout <<"*";
-        k++;
+        ++k;
     }
-cout <<endl;
+   cout << endl;
       
   }
-
-
-  
-  }
+  return 0;
+}
