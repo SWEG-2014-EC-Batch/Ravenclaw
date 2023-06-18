7ef09497f8a8a65cff7046ac98248aa021deeddf
Meronkassahun <125358444+Meronkassahun@users.noreply.github.com>
2023-06-18 20:02:36 +0300

    Create Name.cpp

---
+++ FOP-II/Meron kassahun/Classworks/Name.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+string name;
+void welcome();
+ int main()
+{
+    cout<<"what is your name\n";
+    welcome();
+    cout<<"hello,"<<name<<" "<<"welcome to m.p"<<endl;  
+    
+} 
+void welcome(){
+cin>>name;
+}
