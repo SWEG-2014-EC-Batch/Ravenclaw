921f65a42c18af09cb079c6952ca4e11df64b353
Meronkassahun <125358444+Meronkassahun@users.noreply.github.com>
2023-06-18 19:41:19 +0300

    Create Exercise 4.cpp

---
+++ FOP-II/Meron kassahun/Homework 1/Exercise 4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+double getproduct(int num1, double num2);
+int main(){
+    int firstnum;
+    double secondnum;
+    cout<<"enter the first number\n";
+    cin>>firstnum;
+    cin>>secondnum;
+    cout<<"enter the second number\n";
+    double product=getproduct(firstnum,secondnum);
+    cout<<"the ptoduct of the numbers is"<<product<<endl;
+    return 0;
+}
+double getproduct(int firstnum, double secondnum){
+    return firstnum*secondnum;
+}
+
