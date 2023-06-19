350fc8c9d846137d57d9ff4741ce3567fd86c177
Kira Eskender <99871382+kira200336@users.noreply.github.com>
2023-06-19 11:14:48 +0300

    Update exercise_15.cpp

--- FOP-II/Kirubel Eskender/Homework/exercise_15.cpp
+++ FOP-II/Kirubel Eskender/Homework/exercise_15.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cassert>
 
-void scale(double scaler, double num1, double num2);
+void scale(double scaler, double &num1, double &num2);
 
 using namespace std;
 int main() {
+
     double scaler;
     double num1, num2;
     cout << "SCALER" << endl;
@@ -13,8 +15,10 @@ int main() {
     cin >> num1;
     cin >> num2;
     scale(scaler, num1, num2);
+
 }
-void scale(double scaler, double num1, double num2){
+void scale(double scaler, double &num1, double &num2){
+    assert(scaler != 0);
     cout << "the un-scaled values are " << num1 << " and " << num2 << endl;
     num1 *= scaler;
     num2 *= scaler;
