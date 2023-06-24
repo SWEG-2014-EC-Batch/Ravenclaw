932591cde5dd5e2509374aef4d205d9231c033f9
Meron Getaneh <124381940+meronheron@users.noreply.github.com>
2023-06-24 14:58:26 +0300

    Add files via upload

---
+++ FOP-II/Meron Getaneh/homework3/user_defined_header_file.h
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+//a
+void count_vowels_consonants(); 
+
+//b
+string reverse_string();
+   
+//c
+int compare_strings();
+ 
+//d
+string to_uppercase(); 
+
+//e
+string to_lowercase();
+
