6c2f0ee2dc2858a668ede9813828dfd57eb74e30
Meronkassahun <125358444+Meronkassahun@users.noreply.github.com>
2023-07-12 00:27:17 +0400

    Update phone no.cpp

--- FOP-II/Meron kassahun/structure/phone no.cpp
+++ FOP-II/Meron kassahun/structure/phone no.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+struct Phone {
+    int areaCode;
+    int exchange;
+    int number;
+};
+
+void displayPhone(const Phone& p) {
+    cout << "(" << p.areaCode << ") " << p.exchange << "-" << p.number << endl;
+}
+
+int main() {
+    Phone phone1 = { 212, 767, 8900 }; 
+
+    Phone phone2;
+    cout << "Enter the area code: ";
+    cin >> phone2.areaCode;
+    cout << "Enter the exchange: ";
+    cin >> phone2.exchange;
+    cout << "Enter the number: ";
+    cin >> phone2.number;
+
+    // Display both phone numbers
+    cout << "Phone 1: ";
+    displayPhone(phone1);
+    cout << "Phone 2: ";
+    displayPhone(phone2);
+
+    return 0;
+}
