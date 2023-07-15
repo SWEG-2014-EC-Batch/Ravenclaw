3d2d4bb4871ec88a2865d38ffd7ad4207e7c159c
Mahlet Assefa <mahletassefa07@gmail.com>
2023-07-15 10:07:40 -0700

    user defined structure library

---
+++ FOP-II/project1/structure/hallowpattern_struct.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#ifndef HALLOWPATTERN_STRUCT_H
+#define HALLOWPATTERN_STRUCT_H
+
+struct Triangle {
+        int num_alphabet; 
+      };
+
+Triangle triangle;
+
+void displayUpperTriangle(Triangle);
+void displayLowerTriangle(Triangle);
+#endif
