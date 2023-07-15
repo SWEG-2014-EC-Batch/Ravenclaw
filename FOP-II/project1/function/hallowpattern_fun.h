027b5be9f0aa2d4b511da6312d62721a50541ed0
Mahlet Assefa <mahletassefa07@gmail.com>
2023-07-15 10:01:03 -0700

    user defined libraries

---
+++ FOP-II/project1/function/hallowpattern_fun.h
@@ -0,0 +1,9 @@
+#include<iostream>
+#ifndef HALLOWPATTERN_FUN_H
+#define HALLOWPATTERN_FUN_H
+
+int row, column, num_alphabet;
+
+void displayUpperTriangle(int num_alphabet);
+void displayLowerTriangle(int num_alphabet);
+#endif
