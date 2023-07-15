833c2f700f6b05b1f29eed7fcfc0e4a1f2f9f680
Mahlet Assefa <mahletassefa07@gmail.com>
2023-07-15 09:59:13 -0700

    diamond_hollow_pattern_with_function.cpp

--- FOP-II/project1/function/diamond_hollow_pattern.cpp
+++ FOP-II/project1/function/diamond_hollow_pattern.cpp
@@ -1,1 +1,49 @@
 
+#include<iostream>
+#include "HALLOWPATTERN_FUN.H"
+using namespace std;
+
+
+int main()
+{
+    cout << "Enter Largest Alphabet Value to display(i.e: H=8):"<<endl<<"Also make sure to enter below 26 to provide all alphabet pattern"<<endl;
+    cin >> num_alphabet;
+    if (num_alphabet<27){
+       displayUpperTriangle(num_alphabet);
+       displayLowerTriangle(num_alphabet);
+    }
+    else{
+       return 0;   
+    } 
+  }
+    
+void displayUpperTriangle(int num_alphabet){
+    //nested for loop to display the upper triangled characters
+     for (int row = 0; row < num_alphabet; row++) {
+        for (int column = 65; column < 64 + (2 * num_alphabet); column++) {
+            if (column <= (64 + num_alphabet) - row)
+                cout << (char)column;
+            else if (column >= (64 + num_alphabet) + row)
+                cout << (char)((64 + num_alphabet) - (column % (64 + num_alphabet)));//portray the reversed characters
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}  
+
+void displayLowerTriangle(int num_alphabet){
+     //another for loop to display the lower triangled characters
+    for (int row = num_alphabet-2; row >= 0; row--) {
+        for (int column = 65; column < 64 + (2 * num_alphabet); column++) {
+            if (column >= (64 + num_alphabet) + row)
+                cout << (char)((64 + num_alphabet) - (column % (64 + num_alphabet)));//for reversed characters
+            else if (column <= (64 + num_alphabet) - row)
+                cout << (char)column;
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+    
