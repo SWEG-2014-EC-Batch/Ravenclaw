48b803865454cf4d479bd9e94cf55513fb16cb79
Meron Getaneh <124381940+meronheron@users.noreply.github.com>
2023-09-04 22:08:04 +0300

    Add files via upload

---
+++ FOP-II/STL/mutlimapexm1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+//declaring a mutimap with a key string and value also string
+multimap<string,string>subjects;
+subjects.insert(make_pair("helen","economics"));
+subjects.insert(make_pair("mahlet","psychology"));
+subjects.insert(make_pair("linda","philosophy"));
+subjects.insert(make_pair("helen","philosophy"));
+//print all associated subjects with helen
+cout << "the subjects helen learns : ";
+    auto range = subjects.equal_range("helen");
+    for (auto it = range.first; it != range.second; ++it) {
+        cout << it->second << ", ";
+    }
+    cout <<endl; 
+
+}
+
