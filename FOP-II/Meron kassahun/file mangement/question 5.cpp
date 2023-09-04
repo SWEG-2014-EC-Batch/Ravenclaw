78f9c33993209479bd88f24130337786f6804954
Meronkassahun <125358444+Meronkassahun@users.noreply.github.com>
2023-09-04 23:46:29 +0400

    Create question 5.cpp

---
+++ FOP-II/Meron kassahun/file mangement/question 5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream inputFile("averagenum.txt"); 
+
+    if (!inputFile.is_open()) {
+        cout << "Failed to open file." << endl;
+        return 1;
+    }
+
+    int groupSize;
+    while(inputFile >> groupSize) {
+        int sum = 0;
+        int num;
+        for (int i = 0; i < groupSize; i++) {
+            inputFile >> num;
+            sum += num;
+        }
+        double average = static_cast<double>(sum) / groupSize;
+        cout << "Average of group with " << groupSize << " numbers: " << average << endl;
+    }
+
+    inputFile.close();
+    return 0;
+}
